e5c2d1db38b7333588749a6b537d1b0e51848f3a
Harshit Tanwar <harshittanwar0@gmail.com>
2025-01-29 00:13:38 +0530

    change in ui of expense dets

--- src/Components/ExpenseDetailsModal.jsx
+++ src/Components/ExpenseDetailsModal.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ExpenseDetailsModal = ({expense , onClose}) => {
     return (
         <div className="modal fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center z-10">
-            <div className="modal-content bg-zinc-100 w-1/3 h-1/2 p-5 rounded-lg">
+            <div className="modal-content bg-zinc-100 w-[40%] h-1/2 p-5 rounded-lg">
                 <h2 className='text-center text-2xl py-2 font-bold'>Expense Details</h2>
                 <p className='text-xl py-1'><strong>Title:</strong> {expense.title}</p>
                 <p className='text-xl py-1'><strong>Category:</strong> {expense.category}</p>
